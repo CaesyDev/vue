785132ad064e1276aa28e2fc0ec31fe8fc555594
CaesyDev <kenechukwuokafor7@gmail.com>
2023-11-28 15:02:41 +0100

    initial vue js commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello world</h1>
+    <PageViewer :pages="navNames" :active-pages="activePages"></PageViewer>
+    <NavBar :updateIndex="function(index){ activePages = index;}"></NavBar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageViewer from './components/PageViewer';
+import NavBar from './components/NavBar';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        PageViewer,
+        NavBar
+    },
+    data() {
+        return {
+            activePages: 0,
+            isLightOn: false,
+            theme: 'danger',
+            link: "google.com",
+            name: "Howell",
+            counts: [1, 2, 3, 4],
+            navNames: [
+                {
+                    name: "Home", link: "home.html", content: "This is the home page"
+                }
+                ,
+                {
+                    name: "About", link: "about.html", content: "This is the about page"
+                },
+                {
+                    name: "License", link: "license.html", content: "This is the license page"
+                },
+                {
+                    name: "Register", link: "register.html", content: "This is the registration page"
+                }
+            ]
+        }
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="col-lg-12 mb-4">
+        <div class="main-button-red">
+            <h2>Page viewer template</h2>
+            <h3>{{ pages[activePages].content }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pages', 'activePages'],
+}
+</script>
